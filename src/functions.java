24ea1340291ae11ce67bff048aad65cfcbdf2125
shubh2605 <singh10143shubham@gmail.com>
2026-05-25 09:49:42 +0530

    Pushing

--- src/functions.java
+++ src/functions.java
@@ -1,24 +1,3 @@
-//    fxn is a block of code use to run specific task
-//    features:
-//    increase readibility,
-//    inhance reusablility
-//in java,we cant declare a fxn inside a fxn
-//Fxn and their local variable are created in stack memory
-//stack contains the stack frames made by fxn which contains its variable
-//object,refrence ,runtime data are stored in heap momory
-
-//Static variables must be declared outside of method.
-// If you try to use the static keyword to declare a variable inside a method, the compiler will throw an "illegal start of expression" error.
-
-//public static void statisStudy(){
-//
-//        int d=190;       local scope variable
-//        if(i%2==0){
-//            itn r=101;   block scope variable
-//        }
-//}
-
-
 public class functions {
 
     public static void sum(int a,int b,int c){
@@ -31,12 +10,14 @@ public class functions {
         }
         return true;
     }
+
     public static long nfactorial(long n){
         if(n==1 || n==0)return 1;
         long val =n*nfactorial(n-1);
         return val;
     }
-    static long nPr(long n, long r) {
+
+    public static long nPr(long n, long r) {
         // code here
         if(r>n)return 0;
         // code here
@@ -44,11 +25,55 @@ public class functions {
         return result;
     }
 
+    public static void printNum(int n){
+        if(n==1) {
+            System.out.println(1);
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
+
+    public static int evenSum(int st,int end){
+
+        int sum=0;
+//        for(int i=st;i<=end;i++){
+//            if(i%2==0)sum+=i;
+//        }
+        if(end<st){
+            return sum;
+        }
+        if(end%2==0)sum+=end;
+
+        return evenSum(st,end-1);
+    }
+
+    public static String greetting(String name){
+        return "Hello "+ name;
+    }
+
+    public static boolean LastsecEven(int n){
+        int count=0;
+        while(n>0){
+            count++;
+            int num=n%10;
+            n=n/10;
+            if(count==2 && num%2==0)return true;
+        }
+        return false;
+    }
+
+
+
     public static void main(String[] args){
 //        add(2,3//these r called argumnets);
 //        sum(2,4,6);
 //        System.out.println(isPrime(9));
-        System.out.println(nPr(33,9));
+//        System.out.println(nPr(33,9));
+//        printNum(10);
+//        System.out.println(evenSum(2,9));
+//        System.out.println(LastsecEven(124365));
+        System.out.println(greetting("Shubham"));
 
     }
 }
